786b05bb59f4edc1d1da8daf7a680a5ff25b0998
Ankur Gangwar <ankurgangwar@Ankurs-MacBook-Air.local>
2026-01-12 12:58:55 +0530

    Added jwt verification in auth middleware, created a POST profile endpoint

--- src/routes/player.routes.js
+++ src/routes/player.routes.js
@@ -3,6 +3,7 @@ const auth = require("../middleware/auth");
 const ctrl = require("../controllers/player.controller");
 
 router.get("/profile", auth, ctrl.getProfile);
+router.post("/profile", auth, ctrl.getProfile);
 router.post("/save", auth, ctrl.saveProfile);
 router.get("/leaderboard", ctrl.getLeaderboard);
 router.post("/nft-pass", auth, ctrl.activateNftPass);
